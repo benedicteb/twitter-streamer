09aea04aefbfb7606ab6896b42e52fe60b168848
Benedicte Emilie Brækken <b@brkn.io>
2020-04-15 14:46:58 +0200

    Add eventsource listener

--- twitter-streamer-front/src/App.tsx
+++ twitter-streamer-front/src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [tweets, setTweets] = useState<Tweet[]>([]);
+
+  useEffect(() => {
+    const eventSource = new EventSource("/tweets/subscribe");
+
+    eventSource.onmessage = e => {
+      console.log(e);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tweets</h1>
+
+      <ul>
+        {tweets.map(tweet => (
+          <li key={tweet.text}>{tweet.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ twitter-streamer-front/src/types.d.ts
@@ -0,0 +1,3 @@
+interface Tweet {
+  text: string;
+}
