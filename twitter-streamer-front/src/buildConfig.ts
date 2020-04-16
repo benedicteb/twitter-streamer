105d8016fddd5e69474f61f5094384d4ccb6a895
Benedicte Emilie Brækken <b@brkn.io>
2020-04-16 13:31:19 +0200

    Make sure tweet limit is int

--- twitter-streamer-front/src/buildConfig.ts
+++ twitter-streamer-front/src/buildConfig.ts
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 const config = {
   backendHost: process.env["BACKEND_HOST"] || "http://localhost:3000",
-  maxTweetsToList: process.env["MAX_TWEETS_TO_LIST"] || 10
+  maxTweetsToList: parseInt(process.env["MAX_TWEETS_TO_LIST"] || "10")
 };
 
 fs.writeFileSync("src/config.json", JSON.stringify(config));
