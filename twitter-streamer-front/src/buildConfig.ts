5eb22907ce46607ea33c9c57ae07633c10d369bf
Benedicte Emilie Brækken <b@brkn.io>
2020-04-16 13:27:21 +0200

    Fix bug where buildConfig failed to run
    Due to ts-node and tsconfig not playing nice with each other.

--- twitter-streamer-front/src/buildConfig.ts
+++ twitter-streamer-front/src/buildConfig.ts
@@ -1,8 +1,10 @@
-import { writeFileSync } from "fs";
+const fs = require("fs");
 
 const config = {
   backendHost: process.env["BACKEND_HOST"] || "http://localhost:3000",
   maxTweetsToList: process.env["MAX_TWEETS_TO_LIST"] || 10
 };
 
-writeFileSync("../src/config.json", JSON.stringify(config));
+fs.writeFileSync("src/config.json", JSON.stringify(config));
+
+export {};
